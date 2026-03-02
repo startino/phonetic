28152d5c0d0f9b6741ae2d35ca7a963457534d9f
Jonas Lindberg <eksno@protonmail.com>
2026-03-02 11:27:10 +0100

    debug: fix log import name

--- phonetic/ui/settings.py
+++ phonetic/ui/settings.py
@@ -251,8 +251,8 @@ class SettingsWindow(ctk.CTkToplevel):
     def _on_record_key_press(self, event: tk.Event) -> str:
         keysym = event.keysym
 
-        from ..log import _log
-        _log(f"KEY keysym={keysym!r} keycode={event.keycode} char={event.char!r} state={event.state:#x}")
+        from ..log import log
+        log(f"KEY keysym={keysym!r} keycode={event.keycode} char={event.char!r} state={event.state:#x}")
 
         # Escape cancels
         if keysym == "Escape":
