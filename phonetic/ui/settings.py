fa1cea84d5df0e576f0728e8d123d33be4259a83
Jonas Lindberg <eksno@protonmail.com>
2026-03-02 11:20:15 +0100

    debug: log keycode values in hotkey recorder

--- phonetic/ui/settings.py
+++ phonetic/ui/settings.py
@@ -251,6 +251,9 @@ class SettingsWindow(ctk.CTkToplevel):
     def _on_record_key_press(self, event: tk.Event) -> str:
         keysym = event.keysym
 
+        from ..log import _log
+        _log(f"KEY keysym={keysym!r} keycode={event.keycode} char={event.char!r} state={event.state:#x}")
+
         # Escape cancels
         if keysym == "Escape":
             self._stop_hotkey_record()
